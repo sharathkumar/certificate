63fd0d28d3d88889463fb106e6efd6098edf814b
SARATHKUMAR K.S <sarathksuresh001@gmail.com>
2016-10-03 16:17:07 +0530

    Catalog First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catalog::Application.config.secret_key_base = '2dda20be7bce5059dd21278edd445f81948a75fd9a2358c7ba50df85bca4de2b7dd20e6c91d2ea203757f3101d6b9b92ca506680d83714e9fff61f9a8423554d'
---
+++ config/routes.rb
@@ -0,0 +1,69 @@
+Catalog::Application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  resources :cerificates, only: [:show]
+
+  post 'register', to: 'registration#register', as: :tool_registration
+  post 'reregister', to: 'registration#register', as: :tool_reregistration
+  post 'submit_capabilities', to: 'registration#save_capabilities', as: 'save_capabilities'
+  get 'submit_proxy/:registration_uuid', to: 'registration#submit_proxy', as: 'submit_proxy'
+
+  get 'cerificates', to: 'certificates#certificates'
+  get 'courses', to: 'certificates#courses'
+
+  mount RailsLti2Provider::Engine => "/rails_lti2_provider"
+
+end
